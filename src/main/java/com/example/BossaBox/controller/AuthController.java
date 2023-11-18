2bf32bfbabc9fa8505e1d35352bafeedd0a51745
reidn3r <reidnersteam@hotmail.com>
2023-11-18 19:19:55 -0300

    feat: api docs. added

--- src/main/java/com/example/BossaBox/controller/AuthController.java
+++ src/main/java/com/example/BossaBox/controller/AuthController.java
@@ -6,13 +6,16 @@ import com.example.BossaBox.DTO.RegisterDTO;
 import com.example.BossaBox.domain.User.UserModel;
 import com.example.BossaBox.infra.security.TokenService;
 import com.example.BossaBox.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/v1/auth")
+@SecurityRequirement(name="Bearer Auth")
+@Tag(name="Authentication Controller")
 public class AuthController {
 
     @Autowired
@@ -32,6 +37,19 @@ public class AuthController {
     @Autowired
     private TokenService tokenService;
 
+    @Operation(
+            description = "Endpoint que cria novos usuários",
+            responses = {
+                    @ApiResponse(
+                            description = "Sucesso",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "Falha. Usuário ja registrado",
+                            responseCode = "409"
+                    )
+            }
+    )
     @PostMapping("/register")
     public ResponseEntity<Object> registerController(@RequestBody @Valid RegisterDTO data){
     if(userRepo.findByUsername(data.username()) != null) return ResponseEntity.status(HttpStatus.CONFLICT).body("Usuario ja cadastrado.");
@@ -48,6 +66,20 @@ public class AuthController {
     userRepo.save(newRegister);
     return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @Operation(
+            description = "Endpoint que faz a autenticação do usuário",
+            responses = {
+                    @ApiResponse(
+                            description = "Sucesso",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Nao autorizado/Token inválido",
+                            responseCode = "403"
+                    )
+            }
+    )
 
     @PostMapping("/login")
     public ResponseEntity<Object> loginController(@RequestBody @Valid AuthenticationDTO data) {
